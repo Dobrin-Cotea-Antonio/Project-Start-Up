f736f9f7191324dda80455dc7bc903388506fc3c
Dobrin-Cotea-Antonio <534902@student.saxion.nl>
2023-11-24 19:30:54 +0100

    Clone Ability

---
+++ Assets/CloneAndInvisAbility.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneAndInvisAbility : Ability{
+
+    [Header("Data")]
+    [SerializeField] GameObject clonePrefab;
+    [SerializeField] Transform modelRotation;
+    [SerializeField] float cloneTransparency;
+
+
+    public override void UseAbility(){
+
+        if (Time.time - lastTimeUsed >= cooldown) {
+            StartCoroutine(CreateClone());
+        }
+
+    }
+
+
+    IEnumerator CreateClone() {
+
+        lastTimeUsed = Time.time;
+        GameObject clone = Instantiate(clonePrefab, transform.position, modelRotation.rotation);
+        clone.GetComponent<ChangeTransparency>().SetTransparency(cloneTransparency);
+
+        yield return new WaitForSeconds(abilityDuration);
+
+        Destroy(clone);
+    
+    }
+
+}
